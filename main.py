b81ad4fe4afeea440fb9f4a9020bc87a13e67e67
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-16 12:28:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from os import name
 import zipfile
 
+nameFile = input('File name or folder: ')
+zip = input('Do you want to create(c) or extract(e) the file: ')
+
+nameZip = nameFile + '.zip'
+
+def zipCreate(name):
+      with zipfile.ZipFile(name, 'w') as zip:
+            zip.write(name)
+print('successfully')
+
+def zipExtract(name):
+      with zipfile.ZipFile(name, 'r') as zip:
+            zip.extractall(name)
+print('successfully')
+
+if (zip == 'c'):
+      zipCreate(nameZip)
+elif (zip == 'e'):
+      zipExtract(nameZip)
+else:
+      print('Error')
