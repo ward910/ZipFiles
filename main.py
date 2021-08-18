f29016eb686661d6b35451dcbaa7fb9b1508a35e
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-18 12:00:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-import zipfile
+from subprocess import call
 
+# know the name of the file or folder
 nameFile = input('File name or folder: ')
+# know it's a file or folder
+fileFolder = input('And a file(file) or folder(folder): ')
+# knows that the person who creates a zip or extracts a zip
 zip = input('Do you want to create(c) or extract(e) the file: ')
-
 nameZip = nameFile + '.zip'
 
-def zipCreate(name):
-      with zipfile.ZipFile(name, 'w') as zip:
-            zip.write(name)
-print('successfully')
+def zipCreate(name, fileFolder):
+      # for files    
+      if (fileFolder == 'file'):
+            call(f'zip {nameZip} {name}', shell=True)
+      else:
+      # for folders
+            call(f'zip -r {nameZip} {name}', shell=True)
 
 def zipExtract(name):
-      with zipfile.ZipFile(name, 'r') as zip:
-            zip.extractall(name)
-print('successfully')
+      # extract .zip
+      call(f'unzip {name}', shell=True)
 
 if (zip == 'c'):
-      zipCreate(nameZip)
+      zipCreate(nameFile, fileFolder)
 elif (zip == 'e'):
-      zipExtract(nameZip)
+      zipExtract(nameFile)
 else:
       print('Error')
